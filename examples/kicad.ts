f02fe4a1a244a5b6c172f721d35e59c3e3ae937c
nathsou <nathsoufflet@gmail.com>
2022-05-19 00:46:57 +0200

    Add 74xx components

--- examples/kicad.ts
+++ examples/kicad.ts
@@ -1,31 +1,68 @@
-import { createModule, IO } from '../src';
+import { createModule, createSimulator, IO, Tuple } from '../src';
 import { KiCad } from '../src/export/kicad/kicad';
 import { SExpr } from '../src/export/kicad/s-expr';
-import { binaryAdder74283 } from '../src/modules/74xx';
+import { isolateGates, u74x02, u74x08, u74x32 } from '../src/modules/74xx';
 
-const KICAD_LIBS_DIR = '/Applications/KiCad/KiCad.app/Contents/SharedSupport';
+const KICAD_LIBS_DIR = '/mnt/c/Program Files/KiCad/6.0/share/kicad';
+
+const adder4 = createModule({
+  name: '74xx_full_adder4',
+  inputs: { a: 4, b: 4, cin: 1 },
+  outputs: { s: 4, cout: 1 },
+  connect({ a: [a3, a2, a1, a0], b: [b3, b2, b1, b0], cin: cin0 }, out) {
+    const [nor1, nor2, nor3, nor4] = isolateGates(u74x02());
+    const [nor5, nor6, nor7, nor8] = isolateGates(u74x02());
+    const [and1, and2, and3, and4] = isolateGates(u74x08());
+    const [and5, and6, and7, and8] = isolateGates(u74x08());
+    const [or1, or2, or3, or4] = isolateGates(u74x32());
+
+    const inter0 = nor1(a0, b0);
+    const s0 = nor2(inter0, cin0);
+    const cin1 = or1(and1(inter0, cin0), and2(a0, b0));
+
+    const inter1 = nor3(a1, b1);
+    const s1 = nor4(inter1, cin1);
+    const cin2 = or2(and3(inter1, cin1), and4(a1, b1));
+
+    const inter2 = nor5(a2, b2);
+    const s2 = nor6(inter2, cin2);
+    const cin3 = or3(and5(inter2, cin2), and6(a2, b2));
+
+    const inter3 = nor7(a3, b3);
+    const s3 = nor8(inter3, cin3);
+    const cin4 = or4(and7(inter3, cin3), and8(a3, b3));
+
+    out.s = [s3, s2, s1, s0];
+    out.cout = cin4;
+  }
+});
 
 const top = createModule({
   name: 'lab',
-  inputs: {},
-  outputs: {},
-  connect() {
-    const uAdder = binaryAdder74283();
+  inputs: { a: 4, b: 4 },
+  outputs: { leds: 5 },
+  connect({ a, b }, out) {
+    const uAdder = adder4();
 
-    IO.forward({ gnd: 0, vcc: 1 }, [uAdder]);
+    uAdder.in.cin = 0;
+    uAdder.in.a = a;
+    uAdder.in.b = b;
 
-    uAdder.in.a = [0, 1, 0, 1];
-    uAdder.in.b = [1, 0, 1, 1];
+    out.leds = [uAdder.out.cout, ...uAdder.out.s];
   }
 })();
 
 const main = async () => {
-  const libs = await KiCad.scanLibraries(KICAD_LIBS_DIR);
-  const symbols = await KiCad.collectUsedSymbols(top, libs);
-  const netlist = KiCad.generateNetlist(symbols, top);
-  console.log(SExpr.show(netlist, false));
+  // const libs = await KiCad.scanLibraries(KICAD_LIBS_DIR);
+  // const symbols = await KiCad.collectUsedSymbols(top, libs);
+  // const netlist = KiCad.generateNetlist(symbols, top);
+  // console.log(SExpr.show(netlist, false));
 
-  // console.log(symbols.map(s => [s.symbol, s.pins]));
+  const sim = createSimulator(top);
+
+  sim.input({ a: [0, 0, 0, 1], b: [0, 0, 0, 1] });
+
+  console.log(sim.state.read(top.out.leds));
 };
 
 main();
--- src/modules/74xx.ts
+++ src/modules/74xx.ts
@@ -1,48 +1,151 @@
-import { createModule } from "../core";
-import { logicalNand } from "./gates";
+import { Connection, createModule, Module } from "../core";
+import { logicalAnd, logicalNand, logicalNot, logicalOr, logicalXnor, logicalXor } from "./gates";
 import { adder } from './arith';
+import { Tuple } from "../utils";
+
+// https://en.wikipedia.org/wiki/List_of_7400-series_integrated_circuits
+
+const defaultFootprint = (n: number) => `Package_DIP:DIP-${n}_W7.62mm_Socket`;
+
+const create74xQuadLogicalGatesModule = (id: string, gate: (a: Connection, b: Connection) => Connection) => {
+  return (footprint = defaultFootprint(14)) => createModule({
+    name: `74x${id}`,
+    inputs: { gnd: 1, vcc: 1, a: 4, b: 4 },
+    outputs: { y: 4 },
+    kicad: {
+      symbol: `74xx:74LS${id}`,
+      footprint,
+      pins: {
+        1: 'a1', 2: 'b1', 3: 'y1',
+        4: 'a2', 5: 'b2', 6: 'y2', 7: 'gnd',
+        8: 'y3', 9: 'a3', 10: 'b3',
+        11: 'y4', 12: 'a4', 13: 'b4', 14: 'vcc',
+      },
+    },
+    connect(inp, out) {
+      out.y = [
+        gate(inp.a[0], inp.b[0]),
+        gate(inp.a[1], inp.b[1]),
+        gate(inp.a[2], inp.b[2]),
+        gate(inp.a[3], inp.b[3]),
+      ];
+    },
+  })();
+};
 
 /**
- * Four independent 2-input NAND gates
- * https://assets.nexperia.com/documents/data-sheet/74HC_HCT00.pdf
+ * quad 2-input NAND gates
+ * https://www.ti.com/lit/ds/symlink/sn74ls00.pdf
  */
-export const u7400 = createModule({
-  name: '7400',
-  inputs: { gnd: 1, vcc: 1, a: 4, b: 4 },
-  outputs: { y: 4 },
+export const u74x00 = create74xQuadLogicalGatesModule('00', logicalNand);
+export const quad2InputNandGates74x00 = u74x00;
+
+/**
+ * quad 2-input NOR gates
+ * https://www.ti.com/lit/ds/symlink/sn74ls02.pdf
+ */
+export const u74x02 = create74xQuadLogicalGatesModule('02', logicalOr);
+export const quad2InputNorGates74x02 = u74x02;
+
+/**
+ * quad 2-input AND gates
+ * https://www.ti.com/lit/ds/symlink/sn74ls08.pdf
+ */
+export const u74x08 = create74xQuadLogicalGatesModule('08', logicalAnd);
+export const quad2InputAndGates74x08 = u74x08;
+
+/**
+ * quad 2-input OR gates
+ * https://www.ti.com/lit/ds/symlink/sn74ls32.pdf
+ */
+export const u74x32 = create74xQuadLogicalGatesModule('32', logicalOr);
+export const quad2InputOrGates74x32 = u74x32;
+
+/**
+ * quad 2-input XOR gates
+ * https://www.ti.com/lit/ds/symlink/sn74ls86a.pdf
+ */
+export const u74x86 = create74xQuadLogicalGatesModule('86', logicalXor);
+export const quad2InputXorGates74x86 = u74x86;
+
+/**
+ * quad 2-input XNOR gates
+ * https://archive.org/details/bitsavers_tidataBookogicDataBook_23574286/page/n461/mode/2up
+ */
+export const u74x7266 = create74xQuadLogicalGatesModule('7266', logicalXnor);
+export const quad2InputXnorGates74x7266 = u74x7266;
+
+export const u74x04 = (footprint = defaultFootprint(14)) => createModule({
+  name: '74x04',
+  inputs: { gnd: 1, vcc: 1, a: 6 },
+  outputs: { y: 6 },
   kicad: {
-    symbol: '74xx:74LS00',
-    footprint: 'Package_DIP:DIP-14_W7.62mm_Socket',
+    symbol: '74xx:74LS04',
+    footprint,
     pins: {
-      1: 'a1', 2: 'b1', 3: 'y1',
-      4: 'a2', 5: 'b2', 6: 'y2', 7: 'gnd',
-      8: 'y3', 9: 'a3', 10: 'b3',
-      11: 'y4', 12: 'a4', 13: 'b4', 14: 'vcc',
+      1: 'a1', 2: 'y1', 3: 'a2',
+      4: 'y2', 5: 'a3', 6: 'y3', 7: 'gnd',
+      8: 'y4', 9: 'a4', 10: 'y5',
+      11: 'a5', 12: 'y6', 13: 'a6', 14: 'vcc',
     },
   },
   connect(inp, out) {
     out.y = [
-      logicalNand(inp.a[0], inp.b[0]),
-      logicalNand(inp.a[1], inp.b[1]),
-      logicalNand(inp.a[2], inp.b[2]),
-      logicalNand(inp.a[3], inp.b[3]),
+      logicalNot(inp.a[0]),
+      logicalNot(inp.a[1]),
+      logicalNot(inp.a[2]),
+      logicalNot(inp.a[3]),
+      logicalNot(inp.a[4]),
+      logicalNot(inp.a[5]),
     ];
   },
-});
+})();
 
-export const quad2InputNandGates7400 = u7400;
+export const hexInverters74x04 = u74x04;
+
+export const isolateGates = (
+  gates: Module<{ a: 4, b: 4, gnd: 1, vcc: 1 }, { y: 4 }>,
+  power: { vcc: Connection, gnd: Connection } = { vcc: 1, gnd: 1 }
+) => {
+  gates.in.gnd = power.gnd;
+  gates.in.vcc = power.vcc;
+
+  const asFunc = (index: number) => {
+    let alreadyCalled = false;
+
+    return (a: Connection, b: Connection) => {
+      if (alreadyCalled) {
+        throw new Error(`Isolated 74xx gate called multiple times`);
+      }
+
+      alreadyCalled = true;
+
+      gates.in.a[index] = a;
+      gates.in.b[index] = b;
+
+      return gates.out.y[index];
+    };
+  };
+
+  return [
+    asFunc(0),
+    asFunc(1),
+    asFunc(2),
+    asFunc(3),
+  ] as const;
+};
 
 /**
  * 4-bit binary full adder with fast carry
  * https://www.ti.com/lit/ds/symlink/sn74ls283.pdf
  */
-export const u74283 = createModule({
-  name: '74283',
+export const u74x283 = (footprint = defaultFootprint(16)) => createModule({
+  name: '74x283',
   inputs: { gnd: 1, vcc: 1, a: 4, b: 4, c0: 1 },
   outputs: { s: 4, c4: 1 },
   kicad: {
     symbol: '74xx:74LS283',
-    footprint: 'Package_DIP:DIP-16_W7.62mm_Socket',
+    footprint,
   },
   connect(inp, out) {
     const s = adder(4);
@@ -53,7 +156,7 @@ export const u74283 = createModule({
     out.s = s.out.sum;
     out.c4 = s.out.carryOut;
   },
-});
+})();
 
 // 4-bit binary full adder with fast carry
-export const binaryAdder74283 = u74283;
+export const binaryAdder74x283 = u74x283;
